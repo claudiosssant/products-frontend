408103551b447f97cdd9d671b11df53575899a2e
Claudio <claudiogsssantos@gmail.com>
2024-07-17 02:22:41 -0300

    feat: criando main page dentro do arquivo app.jsx e inserindo um título.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@
 function App() {
   
   return (
-    <div>
-      
-    </div>
+    <main className="bg-green-50 h-screen">
+      <div className="p-6 max-w-4xl mx-auto space-y-5">
+        <h1 className="text-3xl font-bold text-center">
+          Meus produtos 🥊
+        </h1>
+      </div>
+    </main>
   )
 }
 
